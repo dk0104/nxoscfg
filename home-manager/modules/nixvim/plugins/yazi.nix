391aa1e130f76ad199f3e34de50b4ffce5d15dbe
Denis Kesel <denis.keksel@keksel.biz>
2024-09-17 22:58:01 +0400

    yazi configured

--- home-manager/modules/nixvim/plugins/yazi.nix
+++ home-manager/modules/nixvim/plugins/yazi.nix
@@ -2,20 +2,22 @@
   programs.nixvim = {
     plugins.yazi = {
       enable = true;
-
-      keymaps =   {
-        mode = "n";
-        key = "<leader>e";
-        action.__raw = ''
-          function()
-          require('yazi').yazi()
-          end
-        '';
-        options = {
-          desc = "Yazi toggle";
-          silent = true;
-        };
-      };
     };
+
+    keymaps =   [
+      {
+      mode = "n";
+      key = "<leader>e";
+      action.__raw = ''
+        function()
+        require('yazi').yazi()
+        end
+      '';
+      options = {
+        desc = "Yazi toggle";
+        silent = true;
+      };
+    }
+  ];
   };
 }
