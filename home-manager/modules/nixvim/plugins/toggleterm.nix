e469727dac2738299bf92ab0d58ec02be463dae8
Denis Kesel <denis.keksel@keksel.biz>
2024-09-22 23:50:05 +0400

    toggle term added

--- home-manager/modules/nixvim/plugins/toggleterm.nix
+++ home-manager/modules/nixvim/plugins/toggleterm.nix
@@ -1,15 +1,24 @@
 {
   programs.nixvim = {
-    keymaps = [
-      {
-        mode = "n";
-        key = "<leader>ot";
-        action = ":ToggleTerm size=42 direction=horizontal name=scratch";
-        options.silent = true;
-      }];
+    # keymaps = [
+      #   {
+        #     mode = "n";
+        #     key = "<leader>ot";
+        #     action = ":ToggleTerm size=42 direction=horizontal name=scratch<CR>";
+        #     options.silent = true;
+        #   }];
 
-    plugins.toggleterm = {
-      enable = true;
-    };
+        plugins.toggleterm = {
+          enable = true;
+          settings = {
+            direction = "float";
+            float_opts = {
+              border = "curved";
+              height = 30;
+              width = 130;
+            };
+            open_mapping = "[[<leader>ot]]";
+          };
+        };
   };
 }
